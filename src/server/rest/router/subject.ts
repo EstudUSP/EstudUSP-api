ee0a1f99bd7e2f4d46c2d12080252dcaf1bf01ca
vitória lopes <heyvitoria.lopes@gmail.com>
2023-09-29 03:47:35 -0300

    refactor: subject router name

--- src/server/rest/router/subject.ts
+++ src/server/rest/router/subject.ts
@@ -4,7 +4,7 @@ import { inject, injectable } from 'inversify';
 import SubjectController from '../controller/subject';
 
 @injectable()
-export default class AuthRouter {
+export default class SubjectRouter {
   constructor(
     @inject(SubjectController) private readonly subjectController: SubjectController,
   ) {}
